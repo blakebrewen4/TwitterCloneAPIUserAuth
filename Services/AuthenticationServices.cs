da4276871e62931601070e5e8b96eacf035b741a
BlakeBrewen <brewen.blake4@gmail.com>
2023-10-25 21:42:08 -0500

    auth changes

--- Services/AuthenticationServices.cs
+++ Services/AuthenticationServices.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using TwitterCloneAPIUserAuth.Models;
 using TwitterCloneAPIUserAuth.Repositories;
 
@@ -8,11 +14,19 @@ namespace TwitterCloneAPIUserAuth.Services
     {
         private readonly UserRepository _userRepository;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IConfiguration _configuration;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public AuthenticationService(UserRepository userRepository, SignInManager<ApplicationUser> signInManager)
+        public AuthenticationService(
+            UserRepository userRepository,
+            SignInManager<ApplicationUser> signInManager,
+            IConfiguration configuration,
+            UserManager<ApplicationUser> userManager)
         {
             _userRepository = userRepository;
             _signInManager = signInManager;
+            _configuration = configuration;
+            _userManager = userManager;
         }
 
         public async Task<bool> ValidateCredentials(string email, string password)
@@ -28,6 +42,31 @@ namespace TwitterCloneAPIUserAuth.Services
             return result.Succeeded;
         }
 
-        // Add additional methods for token generation, validation, etc...
+        public async Task<string?> GenerateJwtToken(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user == null)
+            {
+                return null;
+            }
+
+            var jwtSettings = _configuration.GetSection("Jwt");
+            var key = Encoding.ASCII.GetBytes(jwtSettings["Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpiryInMinutes"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = jwtSettings["Issuer"],
+                Audience = jwtSettings["Audience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(securityToken);
+        }
     }
 }
